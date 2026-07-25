17412b37cd2873939da973d23c01eb8c625f2df8
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-25 17:38:18 -0300

    refactor: substitui ClienteJaExisteException por exceção específica

--- src/main/java/com/exemplo/meu_primeiro_projeto/exception/ClienteJaExisteException.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.meu_primeiro_projeto.exception;
-
-public class ClienteJaExisteException extends RuntimeException{
-    public ClienteJaExisteException(String mensagem) {
-        super(mensagem);
-    }
-}
