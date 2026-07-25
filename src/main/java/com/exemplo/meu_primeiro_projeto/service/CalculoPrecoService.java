b849cfdac52470736704ef5114034cc3a043d719
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-24 23:45:43 -0300

    refactor: adiciona cálculo de valor total das compras

--- src/main/java/com/exemplo/meu_primeiro_projeto/service/CalculoPrecoService.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/service/CalculoPrecoService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.exemplo.meu_primeiro_projeto.model.ItemCarrinho;
+import com.exemplo.meu_primeiro_projeto.model.ItemCompra;
 import com.exemplo.meu_primeiro_projeto.model.ItemVenda;
 
 @Service
@@ -25,4 +26,10 @@ public class CalculoPrecoService {
                    //ItemVenda::Subtotal
                 .reduce(BigDecimal.ZERO, (total, subtotal) -> total.add(subtotal));
     }                                //, BigDecimal::add));
+
+    public BigDecimal calcularValorTotalCompra(List<ItemCompra> itens) {
+        return itens.stream()
+                .map(ItemCompra::getSubtotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
