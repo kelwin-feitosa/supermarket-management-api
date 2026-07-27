e9d45bae2b1fceac8557bf0edcdbd62a20b82cd4
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-26 23:42:12 -0300

    feat: adicionar consulta de fornecedores ativos

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/FornecedorRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/FornecedorRepository.java
@@ -1,9 +1,12 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.exemplo.meu_primeiro_projeto.model.Fornecedor;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
     boolean existsByCnpj(String cnpj);
+    List<Fornecedor> findByAtivoTrue();
 }
