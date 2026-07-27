2b1db1f266ed474a0655adf7c014d085667c506c
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-27 16:06:33 -0300

    docs: adiciona documentação ao FornecedorRequest

--- src/main/java/com/exemplo/meu_primeiro_projeto/dto/FornecedorRequest.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/dto/FornecedorRequest.java
@@ -1,24 +1,44 @@
 package com.exemplo.meu_primeiro_projeto.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Dados necessários para cadastrar ou atualizar um fornecedor.")
 public record FornecedorRequest(
 
+    @Schema(
+        description = "Nome do fornecedor ou empresa fornecedora.",
+        example = "Distribuidora São Paulo LTDA"
+    )
     @NotBlank(message = "O nome é obrigatório.")
     String nome,
 
+
+    @Schema(
+        description = "CNPJ do fornecedor contendo 14 dígitos.",
+        example = "12345678000199"
+    )
     @NotBlank(message = "O CNPJ é obrigatório.")
     @Pattern(
-        // D de dígitos inteiros, 14 de quantidade de digitos  ^ para começo e $ para fim
-        regexp = "^\\d{14}$",  
+        regexp = "^\\d{14}$",
         message = "O CNPJ deve conter exatamente 14 dígitos."
     )
     String cnpj,
 
+
+    @Schema(
+        description = "Número de telefone para contato do fornecedor.",
+        example = "61999999999"
+    )
     @NotBlank(message = "O telefone é obrigatório.")
-    @Size(min = 10, max = 11)
+    @Size(
+        min = 10,
+        max = 11,
+        message = "Telefone inválido."
+    )
     String telefone
 
-) {}
+) { 
+}
