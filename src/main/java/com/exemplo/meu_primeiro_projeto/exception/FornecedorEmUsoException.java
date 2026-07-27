aed27f1d9da01e303fa8e5b56b24c376f2baf83a
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-26 23:46:11 -0300

    refactor: simplificar regra de encerramento e adicionar listagem de ativos

--- src/main/java/com/exemplo/meu_primeiro_projeto/exception/FornecedorEmUsoException.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.meu_primeiro_projeto.exception;
-
-public class FornecedorEmUsoException extends RuntimeException {
-    public FornecedorEmUsoException(String mensagem) {
-        super(mensagem);
-    }
-}
