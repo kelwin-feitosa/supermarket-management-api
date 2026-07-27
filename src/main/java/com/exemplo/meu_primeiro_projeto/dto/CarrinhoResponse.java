75d2dff4bd11300adf384ecd8093615ac7758c2d
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-07-27 16:06:53 -0300

    docs: adiciona documentação ao CarrinhoResponse

--- src/main/java/com/exemplo/meu_primeiro_projeto/dto/CarrinhoResponse.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/dto/CarrinhoResponse.java
@@ -3,9 +3,18 @@ package com.exemplo.meu_primeiro_projeto.dto;
 import java.math.BigDecimal;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CarrinhoResponse(
+
+    @Schema(description = "Identificador do carrinho", example = "1")
     Long id,
+
+    @Schema(description = "Valor total dos produtos presentes no carrinho", example = "150.00")
     BigDecimal valorTotal,
+
+    @Schema(description = "Lista de produtos adicionados ao carrinho" )
     List<ItemCarrinhoResponse> itens
+
 ) {
 }
