d8fd7d7f8aa5c4556b980c4a42d67e6871f2cd08
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-10 19:08:57 -0300

    feat: adiciona suporte a specifications no produto

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/ProdutoRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.exemplo.meu_primeiro_projeto.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long>, JpaSpecificationExecutor<Produto>  {
     boolean existsByNome(String nome);
 }
