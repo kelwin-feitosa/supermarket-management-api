ad7c0d9824070b3a42945780a0db7551f1860c5a
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-13 22:48:04 -0300

    feat: adicionar Specification de filtro de venda

---
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/specification/VendaSpecification.java
@@ -0,0 +1,55 @@
+package com.exemplo.meu_primeiro_projeto.repository.specification;
+
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.exemplo.meu_primeiro_projeto.dto.filter.VendaFiltro;
+import com.exemplo.meu_primeiro_projeto.model.Venda;
+
+public class VendaSpecification {
+
+    public static Specification<Venda> clienteIgual(Long clienteId) {
+        if (clienteId == null) {
+            return null;
+        }
+
+        return (root, query, criteriaBuilder) ->
+            criteriaBuilder.equal(
+                root.get("cliente").get("id"),
+                clienteId
+            );
+    }
+
+    public static Specification<Venda> dataVendaMaiorOuIgual(LocalDate dataInicio) {
+        if (dataInicio == null) {
+            return null;
+        }
+
+        return (root, query, criteriaBuilder) ->
+            criteriaBuilder.greaterThanOrEqualTo(
+                root.get("dataVenda"),
+                dataInicio
+            );
+    }
+
+    public static Specification<Venda> dataVendaMenorOuIgual(LocalDate dataFim) {
+        if (dataFim == null) {
+            return null;
+        }
+
+        return (root, query, criteriaBuilder) ->
+            criteriaBuilder.lessThanOrEqualTo(
+                root.get("dataVenda"),
+                dataFim
+            );
+    }
+
+    public static Specification<Venda> comFiltro(VendaFiltro filtro) {
+        return Specification.allOf(
+            clienteIgual(filtro.clienteId()),
+            dataVendaMaiorOuIgual(filtro.dataInicio()),
+            dataVendaMenorOuIgual(filtro.dataFim())
+        );
+    }
+}
