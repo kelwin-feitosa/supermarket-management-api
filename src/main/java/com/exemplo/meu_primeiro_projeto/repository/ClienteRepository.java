a08ec09a461468c729f3937eacbe9064e5426f90
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-18 17:44:36 -0300

    feat: adiciona JpaSpecificationExecutor ao repository de clientes

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/ClienteRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/ClienteRepository.java
@@ -1,9 +1,10 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.exemplo.meu_primeiro_projeto.model.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long>, JpaSpecificationExecutor<Cliente>{
     boolean existsByEmail(String email);
 }
