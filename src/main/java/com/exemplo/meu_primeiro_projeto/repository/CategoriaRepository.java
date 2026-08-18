176a13adcc1de8ed7773743d24be90d8db4c5e0f
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-18 17:44:30 -0300

    feat: adiciona JpaSpecificationExecutor ao repository de categorias

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/CategoriaRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/CategoriaRepository.java
@@ -1,9 +1,10 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.exemplo.meu_primeiro_projeto.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>, JpaSpecificationExecutor<Categoria> {
     boolean existsByNome(String nome);
 }
