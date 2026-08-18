e287f2fcbec9984cb82d348541c06c2536b9919f
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-18 17:44:51 -0300

    feat: adiciona JpaSpecificationExecutor ao repository de compras

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/CompraRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/CompraRepository.java
@@ -1,9 +1,10 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.exemplo.meu_primeiro_projeto.model.Compra;
 
-public interface CompraRepository extends JpaRepository<Compra, Long> {
+public interface CompraRepository extends JpaRepository<Compra, Long>, JpaSpecificationExecutor<Compra> {
 
 }
