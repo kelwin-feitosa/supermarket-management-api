bd4201ec25d39dd3ed2d50e90c914dd01d6225f9
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-18 17:44:44 -0300

    feat: adiciona JpaSpecificationExecutor ao repository de fornecedores

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/FornecedorRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/FornecedorRepository.java
@@ -1,12 +1,10 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.exemplo.meu_primeiro_projeto.model.Fornecedor;
 
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long>, JpaSpecificationExecutor<Fornecedor> {
     boolean existsByCnpj(String cnpj);
-    List<Fornecedor> findByAtivoTrue();
 }
