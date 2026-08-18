58f0419b9eac2b7d82f078a51e9f8f2d397681e1
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-18 17:44:57 -0300

    feat: adiciona JpaSpecificationExecutor ao repository de vendas

--- src/main/java/com/exemplo/meu_primeiro_projeto/repository/VendaRepository.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/repository/VendaRepository.java
@@ -1,9 +1,10 @@
 package com.exemplo.meu_primeiro_projeto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.exemplo.meu_primeiro_projeto.model.Venda;
 
-public interface VendaRepository extends JpaRepository<Venda, Long> {
+public interface VendaRepository extends JpaRepository<Venda, Long>, JpaSpecificationExecutor<Venda>{
 
 }
