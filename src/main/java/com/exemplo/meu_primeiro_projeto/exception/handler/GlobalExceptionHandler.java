630fbda969cb836884a73d5ed3a479bf017eff1c
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-08-20 12:02:20 -0300

    refactor: remove import desnecessario

--- src/main/java/com/exemplo/meu_primeiro_projeto/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.exemplo.meu_primeiro_projeto.exception.handler;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
