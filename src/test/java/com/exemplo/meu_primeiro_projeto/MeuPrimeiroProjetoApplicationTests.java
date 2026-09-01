0e111b2424a5f46caeeeabc600383334f8307ec9
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-09-01 16:35:13 -0300

    test: configura profile de teste no teste de contexto

--- src/test/java/com/exemplo/meu_primeiro_projeto/MeuPrimeiroProjetoApplicationTests.java
+++ src/test/java/com/exemplo/meu_primeiro_projeto/MeuPrimeiroProjetoApplicationTests.java
@@ -2,13 +2,14 @@ package com.exemplo.meu_primeiro_projeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MeuPrimeiroProjetoApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// Verifica se o contexto da aplicação Spring inicia corretamente.
-	}
-
+    @Test
+    void contextLoads() {
+		
+    }
 }
