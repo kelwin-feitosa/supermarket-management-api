a7f0980cb6dfc20e39f28f6208458fc312538fc3
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-09-01 16:32:33 -0300

    refactor: simplifica CarrinhoService e adiciona transações

--- src/main/java/com/exemplo/meu_primeiro_projeto/service/CarrinhoService.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/service/CarrinhoService.java
@@ -8,48 +8,31 @@ import org.springframework.stereotype.Service;
 import com.exemplo.meu_primeiro_projeto.dto.request.ItemCarrinhoRequest;
 import com.exemplo.meu_primeiro_projeto.dto.response.CarrinhoResponse;
 import com.exemplo.meu_primeiro_projeto.exception.CarrinhoNaoEncontradoException;
-import com.exemplo.meu_primeiro_projeto.exception.ClienteNaoEncontradoException;
 import com.exemplo.meu_primeiro_projeto.exception.ItemCarrinhoNaoEncontradoException;
 import com.exemplo.meu_primeiro_projeto.exception.ProdutoNaoEncontradoException;
 import com.exemplo.meu_primeiro_projeto.mapper.CarrinhoMapper;
 import com.exemplo.meu_primeiro_projeto.model.Carrinho;
-import com.exemplo.meu_primeiro_projeto.model.Cliente;
 import com.exemplo.meu_primeiro_projeto.model.ItemCarrinho;
 import com.exemplo.meu_primeiro_projeto.model.Produto;
 import com.exemplo.meu_primeiro_projeto.repository.CarrinhoRepository;
-import com.exemplo.meu_primeiro_projeto.repository.ClienteRepository;
 import com.exemplo.meu_primeiro_projeto.repository.ItemCarrinhoRepository;
 import com.exemplo.meu_primeiro_projeto.repository.ProdutoRepository;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CarrinhoService {
 
     private final CarrinhoRepository carrinhoRepository;
-    private final ClienteRepository clienteRepository;
     private final ProdutoRepository produtoRepository;
     private final ItemCarrinhoRepository itemCarrinhoRepository;
     private final EstoqueService estoqueService;
     private final CalculoPrecoService calculoPrecoService;
     private final CarrinhoMapper mapper;
 
-    public CarrinhoService(CarrinhoRepository carRepository, ClienteRepository cliRepository, ProdutoRepository produtoRepository, ItemCarrinhoRepository itemCarrinhoRepository, EstoqueService estoqueService, CalculoPrecoService calculoPrecoService, CarrinhoMapper mapper) {
-        this.carrinhoRepository = carRepository;
-        this.clienteRepository = cliRepository;
-        this.produtoRepository = produtoRepository;
-        this.itemCarrinhoRepository = itemCarrinhoRepository;
-        this.estoqueService = estoqueService;
-        this.calculoPrecoService = calculoPrecoService;
-        this.mapper = mapper;
-    }
-
-    public CarrinhoResponse criarCarrinho(Long idCliente) {
-        Carrinho carrinho = new Carrinho(verificarCliente(idCliente));
-
-        carrinho = carrinhoRepository.save(carrinho);
-
-        return mapper.toResponse(carrinho, BigDecimal.ZERO);
-    }
-
+    @Transactional
     public CarrinhoResponse adicionarItem(ItemCarrinhoRequest request) {
         Carrinho carrinho = verificarCarrinho(request.carrinhoId());
         Produto produto = verificarProduto(request.produtoId());
@@ -79,6 +62,7 @@ public class CarrinhoService {
         return mapper.toResponse(carrinho, calculoPrecoService.calcularValorTotal(carrinho.getItens()));
     }
 
+    @Transactional
     public void removerItem(ItemCarrinhoRequest request) {
         Carrinho carrinho = verificarCarrinho(request.carrinhoId());
         ItemCarrinho itemCarrinho = verificarItem(carrinho, request.produtoId());
@@ -86,6 +70,7 @@ public class CarrinhoService {
         carrinho.getItens().remove(itemCarrinho);
     }
 
+    @Transactional
     public CarrinhoResponse limparCarrinho(Long idCarrinho) {
         Carrinho carrinho = verificarCarrinho(idCarrinho);
 
@@ -95,6 +80,7 @@ public class CarrinhoService {
         return mapper.toResponse(carrinho, BigDecimal.ZERO);
     }
 
+    @Transactional
     public CarrinhoResponse alterarQuantidade(ItemCarrinhoRequest request) {
         Carrinho carrinho = verificarCarrinho(request.carrinhoId());
         ItemCarrinho item = verificarItem(carrinho, request.produtoId());
@@ -105,12 +91,6 @@ public class CarrinhoService {
         itemCarrinhoRepository.save(item);
 
         return mapper.toResponse(carrinho, calculoPrecoService.calcularValorTotal(carrinho.getItens()));
-    }
-
-
-    private Cliente verificarCliente(Long idCliente) {
-        return clienteRepository.findById(idCliente)
-                .orElseThrow(() -> new ClienteNaoEncontradoException("Cliente não encontrado."));
     }
 
     private Carrinho verificarCarrinho(Long idCarrinho) {
