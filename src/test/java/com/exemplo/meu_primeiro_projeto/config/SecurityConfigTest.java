3c8642fb9f2863bbd5fb53697b7a224e970be089
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-09-06 10:07:39 -0300

    test: adiciona teste da configuracao de seguranca

---
+++ src/test/java/com/exemplo/meu_primeiro_projeto/config/SecurityConfigTest.java
@@ -0,0 +1,24 @@
+package com.exemplo.meu_primeiro_projeto.config;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityConfigTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deveRetornarUnauthorizedQuandoNaoAutenticado() throws Exception {
+        mockMvc.perform(get("/usuarios"))
+                .andExpect(status().isUnauthorized());
+    }
+}
