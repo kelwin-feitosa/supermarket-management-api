9c6a716a094bd455254ee8a3ca0ba102b2f340b6
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-09-06 10:08:12 -0300

    refactor: migra datas para OffsetDateTime

--- src/main/java/com/exemplo/meu_primeiro_projeto/util/DataHoraUtil.java
+++ src/main/java/com/exemplo/meu_primeiro_projeto/util/DataHoraUtil.java
@@ -1,15 +1,15 @@
 package com.exemplo.meu_primeiro_projeto.util;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
 
 public final class DataHoraUtil {
 
     private DataHoraUtil() { }
 
-    public static LocalDateTime agora() {
-        return LocalDateTime.now(
-            ZoneId.of("America/Sao_Paulo")
+    public static OffsetDateTime agora() {
+        return OffsetDateTime.now(
+                ZoneId.of("America/Sao_Paulo")
         );
     }
 }
